b3865a5c8a757028a1d656d0ed2e8351b60855a4
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2024-12-25 22:57:35 +0300

    created views, urls and setup media settings

--- cloudcart/urls.py
+++ cloudcart/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('electronic', TemplateView.as_view(template_name='electronic.html')),
-    path('fashion', TemplateView.as_view(template_name='fashion.html')),
-    path('jewellery', TemplateView.as_view(template_name='jewellery.html')),
+    path('', include('core.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
