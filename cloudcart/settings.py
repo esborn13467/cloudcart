352bc3c312183595fbf94c48dbe4cff9e5e7bcca
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2024-12-26 00:45:43 +0300

    Login to Admin page using Email instead of username

--- cloudcart/settings.py
+++ cloudcart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
 
     # Custom apps
     'core',
+    'userauths',
 
 ]
 
@@ -143,3 +146,5 @@ JAZZMIN_SETTINGS = {
     'welcome_sign': "Welcome to CloudCart",
 
 }
+
+AUTH_USER_MODEL = 'userauths.User'
