477056d8aba8f075c0b2ae5fc257b0b56798e396
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2024-12-30 01:26:18 +0300

    Add user registration functionality
    Introduce user registration by creating a registration form, view, and URL mapping. Integrate the `userauths` app into the main project, allowing users to register and log in to the system. Included feedback for successful and erroneous submissions via Django messages.

---
+++ userauths/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'userauths'
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    # path('login_view', views.login_view, name='login_view'),
+    # path('logout_view', views.logout_view, name='logout_view'),
+]
