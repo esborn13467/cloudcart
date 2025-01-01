4900039f4f126bb02046268fd6254fb93ca62007
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-01-01 03:28:19 +0300

    Add dynamic product data and localize currency to KSH
    Replaced hardcoded product data with dynamic content from the database. Updated views to include product, featured product, and category data, and integrated it into the template. Standardized currency values from USD to KSH for consistency with localization.

--- core/views.py
+++ core/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 
+from core.models import Product, Category
+
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    product = Product.objects.all().order_by("-date")
+    featured_product = Product.objects.filter(product_status="published", featured=True).order_by("-date")
+    category = Category.objects.all()
+
+    context = {
+        'all_products': product,
+        'featured_products': featured_product,
+        'categories': category
+    }
+
+    return render(request, 'index.html', context)
 
 
 def electronic(request):
@@ -11,4 +24,3 @@ def electronic(request):
 
 def fashion(request):
     return render(request, 'fashion.html')
-
