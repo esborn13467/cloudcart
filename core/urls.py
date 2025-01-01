8e795824486f2c20a9e1c47f7085779f4b7cbf76
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-01-02 01:31:18 +0300

    Add categories view and template for displaying all categories
    Introduced a new `categories` view and corresponding URL to list all categories. Added a `categories.html` template to render the categories with a styled layout. Updated breadcrumb subtitling across views for better navigation consistency.

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('shop-grid/', views.shop_grid, name='shop-grid'),
     path('shoping-cart/', views.shoping_cart, name='shoping-cart'),
     path('category/<str:title>/', views.category, name='category'),
+    path('categories/', views.categories, name='categories'),
 
     ]
--- core/views.py
+++ core/views.py
@@ -26,29 +26,46 @@ def category(request, title):
     context = {
         'category': category,
         'products': product,
-        'breadcrumb_title': title
+        'breadcrumb_title': title,
+        'breadcrumb_subtitle': "Categories/"+title
     }
 
     return render(request, 'category.html', context)
 
 
+def categories(request):
+    product = Product.objects.all()
+
+    context = {
+        'products': product,
+        'breadcrumb_title': "Categories",
+        'breadcrumb_subtitle': "Categories"
+
+    }
+
+    return render(request, 'categories.html', context)
+
+
 def checkout(request):
     context = {
-        'breadcrumb_title': 'Checkout'
+        'breadcrumb_title': 'Checkout',
+        'breadcrumb_subtitle': 'Checkout'
     }
     return render(request, 'checkout.html', context)
 
 
 def contact(request):
     context = {
-        'breadcrumb_title': 'Contact'
+        'breadcrumb_title': 'Contact',
+        'breadcrumb_subtitle': 'Contact'
     }
     return render(request, 'contact.html', context)
 
 
 def shop_details(request):
     context = {
-        'breadcrumb_title': 'Shop-details'
+        'breadcrumb_title': 'Shop-details',
+        'breadcrumb_subtitle': 'Shop-details'
     }
     return render(request, 'shop-details.html', context)
 
@@ -57,13 +74,16 @@ def shop_grid(request):
     product = Product.objects.all().order_by("-date")
     context = {
         'all_products':product,
-        'breadcrumb_title': 'Shop'
+        'breadcrumb_title': 'Shop',
+        'breadcrumb_subtitle': 'Shop'
     }
     return render(request, 'shop-grid.html', context)
 
 
 def shoping_cart(request):
     context = {
-        'breadcrumb_title': 'Shoping Cart'
+        'breadcrumb_title': 'Shoping Cart',
+        'breadcrumb_subtitle': 'Shoping Cart'
+
     }
     return render(request, 'shoping-cart.html', context)
