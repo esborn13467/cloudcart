445105287eaae2682cf04f9099af1d097d4c5dcb
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-01-01 19:39:25 +0300

    Refactor category rendering using a global context processor
    Moved category fetching logic to a context processor for reusability across templates. Updated the base and index templates to use `global_categories` instead of local context. These changes improve maintainability and DRY compliance.

--- core/views.py
+++ core/views.py
@@ -8,12 +8,10 @@ from core.models import Product, Category
 def index(request):
     product = Product.objects.all().order_by("-date")
     featured_product = Product.objects.filter(product_status="published", featured=True).order_by("-date")
-    category = Category.objects.all()
 
     context = {
         'all_products': product,
         'featured_products': featured_product,
-        'categories': category
     }
 
     return render(request, 'index.html', context)
