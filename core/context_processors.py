b97284c5ae08084d1f0e15db511a4a4bf0d01b3e
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-01-02 21:56:33 +0300

    Revert "Add global_products context processor to settings"
    This reverts commit 932670282814f06881ff16d53e67a1956d4d8b76.

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,10 +1,6 @@
-from core.models import Category, Product
+from core.models import Category
 
 
 def global_categories(request):
     categories = Category.objects.all()
     return {'global_categories': categories}
-
-def global_products(request):
-    products = Product.objects.all()
-    return {'global_products': products}
