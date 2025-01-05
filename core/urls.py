d5b853db9140b4cf69acc94f98be952b86c88a6a
OCHOLA-EDDYPHIL <eddyphil.ochola@strathmore.edu>
2025-01-06 00:53:46 +0300

    Add tag-based filtering for products
    Implemented a new `tag_list` view to allow filtering products by tags using slugs. Updated URLs to include a route for tag-based product listing, improving product discoverability by tags.

--- core/urls.py
+++ core/urls.py
@@ -4,15 +4,24 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
+    # Homepage
     path('', views.index, name='index'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('contact/', views.contact, name='contact'),
     path('product/<str:pid>', views.product_details, name='product-details'),
     path('products/', views.product_list, name='product-list'),
-    path('shoping-cart/', views.shoping_cart, name='shoping-cart'),
+
+    # Category
     path('category/<str:title>/', views.category, name='category'),
     path('categories/', views.category_list, name='categories'),
+
+    # Vendor
     path('vendors/', views.vendor_list, name='vendor-list'),
     path('vendor/<str:vid>/', views.vendor_details, name='vendor-details'),
+
+    path('products/tag/<slug:tag_slug>/', views.tag_list, name='tag-list'),
+
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('shoping-cart/', views.shoping_cart, name='shoping-cart'),
+
 
     ]
